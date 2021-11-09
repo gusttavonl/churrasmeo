0fe0b243fd7e38f4ecc2e0f954068de176dfc2a1
GustavoNoronha0 <gustavonoronha0@hotmail.com>
2021-11-09 18:38:41 -0300

    feat: create email validator adapter

---
+++ backend/src/infra/validators/email/email-validator-adapter.ts
@@ -0,0 +1,8 @@
+import { EmailValidator } from '@/validation/protocols'
+import validator from 'validator'
+
+export class EmailValidatorAdapter implements EmailValidator {
+  isValid (email: string): boolean {
+    return validator.isEmail(email)
+  }
+}
