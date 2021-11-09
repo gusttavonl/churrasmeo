50fc511fd9b24f5daf7d184aa9e9460c7b9951be
GustavoNoronha0 <gustavonoronha0@hotmail.com>
2021-11-09 18:41:42 -0300

    feat: create migration of create table participants

---
+++ backend/src/infra/db/postgres/migrations/1636415970335-create-table-participants.ts
@@ -0,0 +1,66 @@
+import { MigrationInterface, QueryRunner, Table, TableForeignKey } from 'typeorm'
+
+export class createTableParticipants1636415970335 implements MigrationInterface {
+  public async up (queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'participants',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()'
+          },
+          {
+            name: 'barbecue_id',
+            type: 'uuid',
+            isNullable: false
+          },
+
+          {
+            name: 'name',
+            type: 'varchar',
+            length: '150'
+          },
+          {
+            name: 'value',
+            type: 'numeric(5,2)'
+          },
+          {
+            name: 'value_suggestion_with_drink',
+            type: 'numeric(5,2)'
+          },
+          {
+            name: 'value_suggestion_without_dring',
+            type: 'numeric(5,2)'
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()'
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()'
+          }
+        ]
+      })
+    )
+    await queryRunner.createForeignKey(
+      'participants',
+      new TableForeignKey({
+        name: 'barbecue_fk',
+        columnNames: ['barbecue_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'barbecue'
+      })
+    )
+  }
+
+  public async down (queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('participants')
+  }
+}
