4d852d266309d3ab04e88eab006eb6fcab71c4ec
GustavoNoronha0 <gustavonoronha0@hotmail.com>
2021-11-13 01:04:08 -0300

    refactor: add value in omit of add barbecue params

--- backend/src/domain/usescasses/barbacue/add-barbecue.ts
+++ backend/src/domain/usescasses/barbacue/add-barbecue.ts
@@ -1,6 +1,6 @@
 import { BarbecueModel } from '@/domain/models/Barbecue'
 
-export type AddBarbecueParams = Omit<BarbecueModel, 'id' | 'created_at' | 'updated_at'>
+export type AddBarbecueParams = Omit<BarbecueModel, 'id' | 'created_at' | 'updated_at' | 'value'>
 export interface AddBarbecue {
   add: (barbecue: AddBarbecueParams) => Promise<BarbecueModel>
 }
